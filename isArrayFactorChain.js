59fd7883252c1687616f5bea07c010295d8d2aa1
Ife-PopOs <sunmolaife@gmail.com>
2024-01-24 22:05:14 -0700

    Added safeguards + Removed unneeded brackets

--- isArrayFactorChain.js
+++ isArrayFactorChain.js
@@ -1,9 +1,7 @@
 const isArrayFactorChain = (arr) => {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i + 1] % arr[i] !== 0) {
-            return false;
-        }
-    }
+    for (let i = 0; i < arr.length && i + 1 < arr.length; i++)
+        if (arr[i + 1] % arr[i] !== 0) return false;
+
     return true;
 }
 
